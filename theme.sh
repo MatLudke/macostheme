574a916298f8753314b5be57710d9abfcb31a80e
MatLudke <61805286+MatLudke@users.noreply.github.com>
2022-07-27 05:03:03 -0300

    Add files via upload

---
+++ theme.sh
@@ -0,0 +1,7 @@
+tar -xf 01-WhiteSur.tar.xz
+tar -xf WhiteSur-Dark.tar.xz
+tar -xf WhiteSur-Light.tar.xz
+mv WhiteSur /usr/share/icons/
+mv WhiteSur-dark /usr/share/icons
+mv WhiteSur-Dark /usr/share/themes/
+mv WhiteSur-Light /usr/share/themes/
